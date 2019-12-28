a4c8bc2a51101d31c6ce5089d325de7d04deb706
obou2002 <34192950+obou2002@users.noreply.github.com>
2019-12-28 12:08:58 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-var getId=Document.getElementById("p").InnerHTML;
+var Zonetxt=document.getElementById("commentaire");
+var tailletext=document.getElementById("taille");
+var choixpolice=document.getElementById("police");
+
+function Boldit(){
+    if(Zonetxt.style.fontWeight=="bold"){Zonetxt.style.fontWeight="normal";}
+    else{Zonetxt.style.fontWeight="bold";}
+}
+
+function Italicthat(){
+    if(Zonetxt.style.fontStyle=="italic"){Zonetxt.style.fontStyle="normal";}
+    else{Zonetxt.style.fontStyle="italic";}
+}
+
+function ToUnderline(){
+    if(Zonetxt.style.textDecoration=="underline"){Zonetxt.style.textDecoration="";}
+    else{Zonetxt.style.textDecoration="underline";}
+}
+
+function tailleup(){Zonetxt.style.fontSize=tailletext.value;}
+
+function Policethat(){Zonetxt.style.fontFamily=choixpolice.value;}
